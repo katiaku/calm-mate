126ecce644e8f52cfb544b184501693f34238ce0
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-08-04 16:39:05 +0200

    Add constants to the project

---
+++ constants/affirmation-images.ts
@@ -0,0 +1,65 @@
+import auroraOne from "../assets/affirmation-images/aurora_1.jpg";
+import auroraTwo from "../assets/affirmation-images/aurora_2.jpg";
+import auroraThree from "../assets/affirmation-images/aurora_3.jpg";
+import auroraFour from "../assets/affirmation-images/aurora_4.jpg";
+
+import baliOne from "../assets/affirmation-images/bali_1.jpg";
+import baliTwo from "../assets/affirmation-images/bali_2.jpg";
+import baliThree from "../assets/affirmation-images/bali_3.jpg";
+import baliFour from "../assets/affirmation-images/bali_4.jpg";
+
+import japanOne from "../assets/affirmation-images/japan_1.jpg";
+import japanTwo from "../assets/affirmation-images/japan_2.jpg";
+import japanThree from "../assets/affirmation-images/japan_3.jpg";
+import japanFour from "../assets/affirmation-images/japan_4.jpg";
+
+import maldivesOne from "../assets/affirmation-images/maldives_1.jpg";
+import maldivesTwo from "../assets/affirmation-images/maldives_2.jpg";
+import maldivesThree from "../assets/affirmation-images/maldives_3.jpg";
+import maldivesFour from "../assets/affirmation-images/maldives_4.jpg";
+
+import mallorcaOne from "../assets/affirmation-images/mallorca_1.jpg";
+import mallorcaTwo from "../assets/affirmation-images/mallorca_2.jpg";
+import mallorcaThree from "../assets/affirmation-images/mallorca_3.jpg";
+import mallorcaFour from "../assets/affirmation-images/mallorca_4.jpg";
+
+import nightSkyOne from "../assets/affirmation-images/night_sky_1.jpg";
+import nightSkyTwo from "../assets/affirmation-images/night_sky_2.jpg";
+import nightSkyThree from "../assets/affirmation-images/night_sky_3.jpg";
+import nightSkyFour from "../assets/affirmation-images/night_sky_4.jpg";
+
+import teaCeremonyOne from "../assets/affirmation-images/tea_ceremony_1.jpg";
+import teaCeremonyTwo from "../assets/affirmation-images/tea_ceremony_2.jpg";
+import teaCeremonyThree from "../assets/affirmation-images/tea_ceremony_3.jpg";
+import teaCeremonyFour from "../assets/affirmation-images/tea_ceremony_4.jpg";
+
+export default {
+    auroraOne,
+    auroraTwo,
+    auroraThree,
+    auroraFour,
+    baliOne,
+    baliTwo,
+    baliThree,
+    baliFour,
+    japanOne,
+    japanTwo,
+    japanThree,
+    japanFour,
+    maldivesOne,
+    maldivesTwo,
+    maldivesThree,
+    maldivesFour,
+    mallorcaOne,
+    mallorcaTwo,
+    mallorcaThree,
+    mallorcaFour,
+    nightSkyOne,
+    nightSkyTwo,
+    nightSkyThree,
+    nightSkyFour,
+    teaCeremonyOne,
+    teaCeremonyTwo,
+    teaCeremonyThree,
+    teaCeremonyFour,
+};
