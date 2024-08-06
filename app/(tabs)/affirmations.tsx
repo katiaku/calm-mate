298c1307db19d4519b74718f4433003e0626145a
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-08-06 12:36:08 +0200

    Add Meditation and Affirmations tabs

---
+++ app/(tabs)/affirmations.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Affirmations = () => {
+  return (
+    <View>
+      <Text>affirmations</Text>
+    </View>
+  );
+};
+
+export default Affirmations;
